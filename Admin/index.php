60a323e9421ed36badbdc726431d06f676528216
Chiduc1611 <Chiduc1611@gmail.com>
2023-11-15 16:45:21 +0700

    update

--- Admin/index.php
+++ Admin/index.php
@@ -6,18 +6,28 @@ require_once "./header.php";
 if (isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'dn':
-            require_once './login.php';
+            require_once './Dangnhap/login.php';
             break;
         case 'dk':
-            require_once './signup.php';
+            require_once './Dangnhap/signup.php';
             break;
         default:
-            require_once './home.php';
+        require_once './home.php';
+            if (isset($_GET['act'])) {
+                switch ($_GET['act']) {
+                    case 'home':
+                        require_once './home.php';
+                        break;
+                    default:
+                        
+                        break;
+                }
+            }
             break;
     }
 } else {
     if (!isset($_SESSION['username'])) {
-        require_once './login.php';
+        require_once './Dangnhap/login.php';
         exit;
     }
 }

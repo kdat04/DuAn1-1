b91d20348edb68659a1a690c1f2fdc71e545dcf6
Chiduc1611 <Chiduc1611@gmail.com>
2023-11-15 15:29:36 +0700

    update

--- Admin/index.php
+++ Admin/index.php
@@ -1,2 +1,24 @@
 <?php
 session_start();
+
+require_once "./header.php";
+
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+        case 'dn':
+            require_once './login.php';
+            break;
+        case 'dk':
+            require_once './signup.php';
+            break;
+        default:
+            break;
+    }
+} else {
+    if (!isset($_SESSION['username'])) {
+        require_once './index.php?action=dn';
+        exit;
+    }
+}
+
+require_once './footer.php';

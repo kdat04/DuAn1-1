27870162dc669ec97e94c59c8ef89832c777e083
Chiduc1611 <Chiduc1611@gmail.com>
2023-11-16 12:23:31 +0700

    update

--- Admin/Phim/add_phim.php
+++ Admin/Phim/add_phim.php
@@ -1,26 +1,26 @@
 <div>
     <div class="page-wrapper">
         <div class="card">
-            <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+            <form class="form-horizontal" action="index.php?action=&act=add_phim" method="post" enctype="multipart/form-data">
                 <div class="card-body" style="width: 1000px;">
                     <h4 class="card-title">Thêm Phim</h4>
                     <div class="form-group row">
                         <label for="fname" class="col-sm-3 text-right control-label col-form-label">Id phim</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="fname" placeholder="Id phim" disabled>
+                            <input type="text" name="id" class="form-control" id="fname" placeholder="Id phim" disabled>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="lname" class="col-sm-3 text-right control-label col-form-label"> Tên phim</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="lname" placeholder="Tên phim">
+                            <input type="text" name="ten_phim" class="form-control" id="lname" placeholder="Tên phim">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="lname" class="col-sm-3 text-right control-label col-form-label">Ảnh phim</label>
                         <div class="col-sm-9">
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="validatedCustomFile" required>
+                                <input type="file" name="anh_phim" class="custom-file-input" id="validatedCustomFile" required>
                                 <label class="custom-file-label" for="validatedCustomFile">Ảnh phim...</label>
                                 <div class="invalid-feedback">Example invalid custom file feedback</div>
                             </div>
@@ -29,31 +29,31 @@
                     <div class="form-group row">
                         <label for="email1" class="col-sm-3 text-right control-label col-form-label">Giá phim</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="email1" placeholder="Giá phim">
+                            <input type="text" name="gia_phim" class="form-control" id="email1" placeholder="Giá phim">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Nhà sản xuất </label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="cono1" placeholder="Nhà sản xuất">
+                            <input type="text" name="nsx" class="form-control" id="cono1" placeholder="Nhà sản xuất">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Ngày phát hành </label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="cono1" placeholder="Ngày phát hành">
+                            <input type="text" name="nph" class="form-control" id="cono1" placeholder="Ngày phát hành">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Mô tả phim</label>
                         <div class="col-sm-9">
-                            <textarea class="form-control"></textarea>
+                            <textarea name="motaphim" class="form-control"></textarea>
                         </div>
                     </div>
                 </div>
                 <div class="border-top">
                     <div class="card-body">
-                        <input type="button" class="btn btn-primary" value="Gửi dữ liệu">
+                        <input type="submit" name="themmoi" class="btn btn-primary" value="Gửi dữ liệu">
                     </div>
                 </div>
             </form>
--- Admin/index.php
+++ Admin/index.php
@@ -59,11 +59,9 @@ if (isset($_GET['action'])) {
                         if (isset($_POST['capnhat']) && ($_POST['capnhat'])) { {
                                 $ma_loai = $_POST['id'];
                                 $ten_loaiphim = $_POST['ten_loaiphim'];
-                                if ($ten_loaiphim == '') {
-                                    $message = "Không được để trắng";
-                                } else {
+                            
                                     loai_update($ma_loai, $ten_loaiphim);
-                                }
+
                             }
                         }
                         $list_danhmuc = loai_select_all();
@@ -72,12 +70,16 @@ if (isset($_GET['action'])) {
                         break;
                     case 'phim':
                         require_once './home.php';
-
+                        
                         require_once './Phim/view_phim.php';
                         require_once './footer-home.php';
                         break;
                     case 'add_phim':
                         require_once './home.php';
+                        if (isset($_POST['themmoi']) && $_POST['themmoi'] > 0) {
+                            $id = $_POST['id'];
+                            $ten_phim = $_POST['ten_phim'];
+                        }
                         require_once './Phim/add_phim.php';
                         require_once './footer-home.php';
                         break;

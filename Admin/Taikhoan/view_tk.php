ad54613c5716a011ace1b1f71838a4fc9332bbc3
kdat2108 <135973747+kdat2108@users.noreply.github.com>
2023-11-20 16:05:08 +0700

    update

--- Admin/Taikhoan/view_tk.php
+++ Admin/Taikhoan/view_tk.php
@@ -14,28 +14,45 @@
                                             <th style="width: 300px;">Họ tên user</th>
                                             <th style="width: 250px;">Số điện thoại</th>
                                             <th style="width: 300px;">Email</th>
-                                            <th>Địa chỉ</th>
+                                            <th style="width: 100px;">Địa chỉ</th>
+                                            <th style="width: 100px;">Năm Sinh</th>
                                             <th style="width: 100px;">Vai trò</th>
+                                            <th></th>
+                                            <th></th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td >1</td>
-                                            <td>Customer Support</td>
-                                            <td></td>
-                                            <td>27</td>
-                                            <td>2011/01/25</td>
-                                            <td></td>
-                                        </tr>
+                                        <?php foreach ($list_users as $user) : ?>
+                                            <tr>
+                                                <td><?= $user['id'] ?></td>
+                                                <td><?= $user['ten_user'] ?></td>
+                                                <td><?= $user['sdt'] ?></td>
+                                                <td><?= $user['email'] ?></td>
+                                                <td><?= $user['diachi'] ?></td>
+                                                <td><?= $user['nam_sinh'] ?></td>
+                                                <td><?= $user['role'] ?></td>
+                                                <td><button class="btn btn-danger" onclick="return confirm('Bạn có muốn xóa sản phẩm không ?')">
+                                                        <a style="color: white;" href="index.php?action=&act=xoa_user&id=<?= $user['id'] ?>">Xoá</a>
+                                                    </button>
+                                                </td>
+                                                <td><button class="btn btn-info" onclick="return confirm('Bạn có muốn sửa sản phẩm không ?')">
+                                                        <a style="color: white;" href="index.php?action=&act=sua_user&id=<?= $user['id'] ?>">Sửa</a>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach ?>
                                     </tbody>
                                     <tfoot>
                                         <tr>
-                                            <th>Id Phim</th>
-                                            <th>Tên Phim</th>
-                                            <th>Ảnh</th>
-                                            <th>Giá</th>
-                                            <th>Ngày Phát Hành</th>
-                                            <th>Vai trò</th>
+                                            <th style="width: 150px;">Id User</th>
+                                            <th style="width: 300px;">Họ tên user</th>
+                                            <th style="width: 250px;">Số điện thoại</th>
+                                            <th style="width: 300px;">Email</th>
+                                            <th style="width: 100px;">Địa chỉ</th>
+                                            <th style="width: 100px;">Năm Sinh</th>
+                                            <th style="width: 100px;">Vai trò</th>
+                                            <th></th>
+                                            <th></th>
                                         </tr>
                                     </tfoot>
                                 </table>
@@ -44,6 +61,13 @@
                     </div>
                 </div>
             </div>
+            <div class="btn_dieuhuong btn btn-success">
+                <a href="./index.php?action=&act=add_user" style="color: white;">Thêm User</a>
+            </div>
+            <?php
+            if (isset($message) && ($message != ""))
+                echo $message;
+            ?>
         </div>
     </div>
 </div>

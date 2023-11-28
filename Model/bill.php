c6369123e2e59e8402ad3c7e94f4469c5e7dc0f0
Chiduc1611 <Chiduc1611@gmail.com>
2023-11-28 19:39:19 +0700

    update

--- Model/bill.php
+++ Model/bill.php
@@ -7,27 +7,39 @@ function bill_insert($ngay_tt, $gia_ghe)
     return pdo_execute_return_interlastid($sql);
 }
 
-function ve_insert($gia_ghe,$ngay_tt,$ghe,$id_user,$id_kgc, $id_bill){
+function ve_insert($gia_ghe, $ngay_tt, $ghe, $id_user, $id_kgc, $id_bill)
+{
     $sql = 'INSERT INTO `ve` (`id`, `gia_ve`, `ngay_dat`, `ghe`, `id_user`, `id_kgc`,`id_bill`) VALUES (NULL,?,?,?,?,?,?)';
-    return pdo_execute_return_interlastid($sql,$gia_ghe,$ngay_tt,$ghe,$id_user,$id_kgc, $id_bill);
+    return pdo_execute_return_interlastid($sql, $gia_ghe, $ngay_tt, $ghe, $id_user, $id_kgc, $id_bill);
 }
 
-function  dich_vu_insert($id_ve,$combo){
+function  dich_vu_insert($id_ve, $combo)
+{
     $sql = 'INSERT INTO `dich_vu` (`id`, `combo`, `id_ve`) VALUES (NULL,?,?)';
-    pdo_execute($sql,$combo,$id_ve);
+    pdo_execute($sql, $combo, $id_ve);
 }
 
-function bill_update($id){
+function bill_update($id)
+{
     $sql = 'UPDATE bill SET tt_bill = 1 WHERE id = ?';
     pdo_execute($sql, $id);
 }
 
-function ve_update($id){
+function bill_updat_gia($id, $gia_ghe)
+{
+    $sql = 'UPDATE bill SET thanh_tien = ?  WHERE id = ?';
+    pdo_execute($sql, $gia_ghe, $id);
+}
+
+
+function ve_update($id)
+{
     $sql = 'UPDATE ve SET tt_ve = 1 WHERE id_bill = ?';
     pdo_execute($sql, $id);
 }
 
-function dv_update($id){
+function dv_update($id)
+{
     $sql = 'UPDATE dich_vu SET tt_dv = 1 WHERE id_ve = ?';
     pdo_execute($sql, $id);
 }
--- Model/ve.php
+++ Model/ve.php
@@ -6,3 +6,8 @@ require_once 'pdo.php';
 //     $sql = "INSERT INTO `phim` (`id`, `ten_phim`, `img_phim`, `img_banner`, `mota`, `nsx`, `nph`,`thoi_luong_phim`  , `cs_danh_gia`,`quocgia`,`dienvien1`,`dienvien2`,`dienvien3`,`tt`, `id_loaiphim`) VALUES (NULL, ?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 //     pdo_execute($sql, $ten_phim, $img_phim, $img_banner_phim, $mota, $nsx, $nph, $thoi_luong_phim, $cs_danh_gia, $qg, $dv1, $dv2, $dv3, $tt, $id_loaiphim);
 // }
+
+function lock_ghe($id_user, $id_kgc, $id_xc, $id_phim){
+    $sql = "SELECT ghe FROM ve JOIN khung_gio_chieu ON ve.id_kgc = khung_gio_chieu.id JOIN xuat_chieu ON xuat_chieu.id = khung_gio_chieu.id_xuat_chieu  WHERE tt_ve = 1 AND id_user='$id_user' AND id_kgc = '$id_kgc' AND xuat_chieu.id = '$id_xc' AND xuat_chieu.id_phim = '$id_phim' ";
+    return pdo_query_one($sql);
+}
